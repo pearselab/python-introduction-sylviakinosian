7e803df0d0e845a71e686800ca6ef40cd4f0172e
Sylvia Kinosian <sylvia@pteris.Home>
2016-11-17 08:00:34 -0700

    pyth0n

---
+++ intro.py
@@ -0,0 +1,12 @@
+#1 Write a loop that prints out the numbers 20 to 10
+
+x = range(10,21)
+
+for i in x:
+    y = x[11 - i]
+    print(x)
+
+#2 Write a list comprehension that returns the numbers from 20 to 10
+
+
+
