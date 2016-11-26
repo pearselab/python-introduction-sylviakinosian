216e291922bcfbe4491124dc6a28d3f776dd1e90
sylviakinosian <sylvia.kinosian@gmail.com>
2016-11-26 08:51:51 -0700

    MY BUBBLES

--- sorting.py
+++ sorting.py
@@ -1,6 +1,12 @@
 #my very own bubble sort
 
-
+#sorts the list len(list)-1 times
+def bubz(list):
+    for j in range(len(list)-1,0,-1):
+        for i in range(j):
+            if list[i] > list[i+1]:
+                list[i], list[i+1] = list[i+1], list[i]
+    return list
 
 #quicksort
 
