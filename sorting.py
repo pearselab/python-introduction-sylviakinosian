cbf4173b2b1d71702956f6fa6b6c243180e97c35
sylviakinosian <sylvia.kinosian@gmail.com>
2016-11-27 11:05:52 -0700

    closing in

--- sorting.py
+++ sorting.py
@@ -10,20 +10,32 @@ def bubz(list):
                 list[i], list[i+1] = list[i+1], list[i]
     return list
 
-#quicksort sorts quickly
+#quicksort
+
+def quicksort(list):
+    bottom = list[0]
+    top = list[len(list)-1]
+    if bottom < top:
+        pivot = partition(list, bottom, top)
+        quicksort(list, bottom, pivot-1)
+        quicksort(list, pivot+1, top)
+    #end if
+#end function
 
 def swap(x, y):
     x, y = y, x
     return x, y
 
-def partition(list):
-    top = list[len(list)-1]
-    bottom = list[0]
-    pivot = top-1
+def partition(list, bottom, top):
+    pivot = top
     x = bottom
-    for i in range(bottom,(top-1)):
-        if list[i] < pivot
+    for i in range(0, len(list)):
+        if list[i] < pivot:
             swap(list[i], list[x])
             x = x + 1
-        end
-        iffffffffffffffffffffffffffffffffffffffffffffvvvvvv                                        ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+        #end if
+    #end for
+    return x
+#end function
+
+
